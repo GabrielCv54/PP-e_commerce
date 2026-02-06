408f0618c00d8f027b0b096745040b25fef358da
Gabriel <gabriel.carvalhovieira123@gmail.com>
2026-02-06 10:53:44 -0300

    add secre_key

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+import os 
+import secrets
 
 db = SQLAlchemy()
 
@@ -7,6 +9,7 @@ def app_config(app):
     app.config['DEBUG'] = True
     app.config['HOST'] = '127.0.0.1'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecompre.db'
 
     db.init_app(app)
