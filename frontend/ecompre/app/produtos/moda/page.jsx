749ef98b74ce5177ad11bd5341e6197b63ad13fa
Gabriel <gabriel.carvalhovieira123@gmail.com>
2026-03-03 16:14:17 -0300

    categoria de moda

--- frontend/ecompre/app/produtos/moda/page.jsx
+++ frontend/ecompre/app/produtos/moda/page.jsx
@@ -1,4 +1,21 @@
+import { getProducts } from "@/lib/api";
+import Link from "next/link";
+import Products from "../ProductList";
 
-export default function Mode(){
+export default async function Mode(){
+    const products = await getProducts()
+
+    return (
+        <>
+        <Link href="/produtos"><img src="/logo_ecompre.png" alt="logo" className="h-25 w-50"/></Link>
+        <article className="grid grid-cols-1 flex-wrap gap-12 md:grid-cols-4">
+        {products.map(prod => {
+           if( prod.setor === 'moda') {
+            return <Products firstProduct={prod} key={prod.id}/>
+           }
+            return null
+        })}</article>
+        </>
+    )
 
 }
