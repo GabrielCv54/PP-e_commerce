3445fdbca62c65fd614beb07e2a91b4bc7df1049
Gabriel <gabriel.carvalhovieira123@gmail.com>
2026-03-03 15:30:14 -0300

    acrescentando tags no return de alimentos

--- frontend/ecompre/app/produtos/alimentos/page.jsx
+++ frontend/ecompre/app/produtos/alimentos/page.jsx
@@ -1,11 +1,23 @@
-export default function Foods(){
+import { getProducts } from "@/lib/api"
+import Products from "../ProductList"
+import Link from "next/link"
+
+export default async function Foods(){
+    const products = await getProducts()
 
     return(
         <>
-        <article className="" id='foods'>
+        <Link href='/produtos'><img src="/logo_ecompre.png" alt="logo"  className="h-25 w-50"/></Link>
+        <article className="grid grid-cols-1 flex-wrap gap-12 md:grid-cols-3 " id='foods'>
+        {products.map(prod => {
+            if(prod.setor === "alimentos"){
+                return <Products key={prod.id} firstProduct={prod} className="h-160"/>
+             }
+                return null
+ 
+        })}
+    </article>   
         
-        <img src="" alt="" />
-        </article>
-        </>
-    )
+     </> 
+     )
 }
